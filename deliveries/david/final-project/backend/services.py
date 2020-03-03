fe286be4ac414a280182d5fd41e64bee3aaf75e1
davidlc-ciandt <davidlc-ciandt@ciandt.com>
2020-03-03 16:57:45 -0300

    project final

--- deliveries/david/final-project/backend/services.py
+++ deliveries/david/final-project/backend/services.py
@@ -1,5 +1,37 @@
-import random
+import datetime
+import uuid
+import yaml
+import json
+import re
 
+from random import randint, random
+
+from repository import save_game, new_game, get_game
+
+MAX_TRIRES = 6
+
+__all__ = ['start_game', 'gess_word', 'reset_game']
+
+def gess_word(_id: str, gess: str):
+	return {}
+	
+def new_id():
+	return uuid.uuid1().int
+
+def reset_game(_id):
+	return 'success'
+
+def start_game(_id):
+
+	word = random.choice(HANGMAN_DATA)
+	find = "_".join([char if char in " " else "" for char in word])
+	start_game = {_id:{
+		'word': word,
+		'find': find,
+		'traies': 0,
+		'result': ''
+	}}
+	return {new_game(start_game)}
 
 HANGMAN_DATA = [
 	"Alura",
@@ -29,9 +61,3 @@ HANGMAN_DATA = [
 	"Journey and Ambiguity",
 	"Lean Startup Week"
 ]
-
-
-word = random.choice(HANGMAN_DATA)
-print(word)
-retorno = "_".join([char if char in " " else "" for char in word])
-print(retorno)
