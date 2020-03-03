b790d91553fc4ec926c963e4939140ee165ab0cb
Arthur de Lapertosa Lisboa <alisboa@ciandt.com>
2020-03-03 11:47:18 -0300

    Projeto inicial

---
+++ deliveries/alisboa/Desafio 10 - Final/backend/repository.py
@@ -0,0 +1,32 @@
+import yaml
+
+__all__ = ['get_game', 'save_game', 'new_game']
+
+def _load():
+    with open('backend/data.yaml', 'r') as file:
+        save = yaml.safe_load(file)
+        if not save['games']:
+            save['games'] = {}
+    return save
+
+
+def _save(data):
+    if data:
+        with open('backend/data.yaml', 'w') as file:
+            return yaml.safe_dump(data, file)
+
+def get_game(_id):
+    data = _load()
+    return data['games'].get(_id, {})
+
+def save_game(_id, game):
+    data = _load()
+    data['games'][_id] = game
+    _save(data)
+    return game
+
+def new_game(new_game):
+    data = _load()  
+    data['games'].update(new_game)
+    _save(data)
+    return new_game
