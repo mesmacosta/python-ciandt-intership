501dc563410562dea33e856179e2aec453c6eb88
Arthur de Lapertosa Lisboa <alisboa@ciandt.com>
2020-03-05 17:02:51 -0300

    Vários testes criados, principalmente para o método gess word - todos funcionais

--- deliveries/alisboa/Desafio 10 - Final/backend/tests_hangman.py
+++ deliveries/alisboa/Desafio 10 - Final/backend/tests_hangman.py
@@ -1,6 +1,6 @@
 import unittest
 
-from services import start_game, gess_word, reset_game, new_id
+from services import start_game, gess_word, reset_game, new_id, MAX_TIME
 from unittest.mock import patch, MagicMock
 
 
@@ -45,14 +45,187 @@ class HangmanTest(unittest.TestCase):
                           }, 200)
                          )
 
+    @patch("time.time")
+    @patch("repository.sessionmaker")
+    def test_gess_word_is_a_correct_word(self, mocking_session_maker, mocking_time):
+        mocking_session_maker.return_value = MagicMock()
+        current_game = Game(_id=31234, date='14/11/1990', word='Alura',
+                            find='_____', tries=0, result='',
+                            time_sec=3213213213,
+                            )
+        mocking_session_maker()().query().filter().one.return_value = current_game
+        mocking_time.return_value = 3213213214
+
+        gess_word_return = gess_word(31234, 'a')
+
+        self.assertEqual(gess_word_return,
+                         {
+                              'game_id': 31234,
+                              'data': {
+                                  'find': 'a___a',
+                                  'tries': 0,
+                                  'result': '',
+                                  'time_sec': 3213213213,
+                                  'date': '14/11/1990',
+                                  'word': None
+                              }
+                          }
+                         )
+
+    @patch("time.time")
+    @patch("repository.sessionmaker")
+    def test_gess_word_is_an_incorrect_word(self, mocking_session_maker, mocking_time):
+        mocking_session_maker.return_value = MagicMock()
+        current_game = Game(_id=31234, date='14/11/1990', word='Alura',
+                            find='_____', tries=0, result='',
+                            time_sec=3213213213,
+                            )
+        mocking_session_maker()().query().filter().one.return_value = current_game
+        mocking_time.return_value = 3213213214
+
+        gess_word_return = gess_word(31234, 'b')
+
+        self.assertEqual(gess_word_return,
+                         {
+                             'game_id': 31234,
+                             'data': {
+                                 'find': '_____',
+                                 'tries': 1,
+                                 'result': '',
+                                 'time_sec': 3213213213,
+                                 'date': '14/11/1990',
+                                 'word': None
+                             }
+                         }
+                         )
+
+
+    @patch("time.time")
+    @patch("repository.sessionmaker")
+    def test_gess_word_losing_game_must_return_lose_and_word(self, mocking_session_maker, mocking_time):
+        mocking_session_maker.return_value = MagicMock()
+        current_game = Game(_id=31234, date='14/11/1990', word='Alura',
+                            find='_____', tries=5, result='',
+                            time_sec=3213213213,
+                            )
+        mocking_session_maker()().query().filter().one.return_value = current_game
+        mocking_time.return_value = 3213213214
+
+        gess_word_return = gess_word(31234, 'b')
+
+        self.assertEqual(gess_word_return,
+                         {
+                             'game_id': 31234,
+                             'data': {
+                                 'find': '_____',
+                                 'tries': 6,
+                                 'result': 'lose',
+                                 'time_sec': 3213213213,
+                                 'date': '14/11/1990',
+                                 'word': 'Alura'
+                             }
+                         }
+                         )
+
+    @patch("time.time")
+    @patch("repository.sessionmaker")
+    def test_gess_word_winning_game_should_return_word(self, mocking_session_maker, mocking_time):
+        mocking_session_maker.return_value = MagicMock()
+        current_game = Game(_id=31234, date='14/11/1990', word='Alura',
+                            find='Alu_a', tries=1, result='',
+                            time_sec=3213213213,
+                            )
+        mocking_session_maker()().query().filter().one.return_value = current_game
+        mocking_time.return_value = 3213213214
+
+        gess_word_return = gess_word(31234, 'r')
+
+        self.assertEqual(gess_word_return,
+                         {
+                             'game_id': 31234,
+                             'data': {
+                                 'find': 'Alura',
+                                 'tries': 1,
+                                 'result': 'win',
+                                 'time_sec': 3213213213,
+                                 'date': '14/11/1990',
+                                 'word': 'Alura'
+                             }
+                         }
+                         )
+
+    @patch("time.time")
+    @patch("repository.sessionmaker")
+    def test_gess_word_winning_game_but_time_is_up_should_lose(self, mocking_session_maker, mocking_time):
+        mocking_session_maker.return_value = MagicMock()
+        current_game = Game(_id=31234, date='14/11/1990', word='Alura',
+                            find='Alu_a', tries=1, result='',
+                            time_sec=3213213213,
+                            )
+        mocking_session_maker()().query().filter().one.return_value = current_game
+        mocking_time.return_value = 3213213214 + MAX_TIME
+
+        gess_word_return = gess_word(31234, 'r')
+
+        self.assertEqual(gess_word_return,
+                         {
+                             'game_id': 31234,
+                             'data': {
+                                 'find': 'Alu_a',
+                                 'tries': 1,
+                                 'result': 'lose',
+                                 'time_sec': 3213213213,
+                                 'date': '14/11/1990',
+                                 'word': 'Alura'
+                             }
+                         }
+                         )
+
+    @patch("time.time")
+    @patch("repository.sessionmaker")
+    def test_gess_word_winning_game_uppercase_letter_should_return_word(self, mocking_session_maker, mocking_time):
+        mocking_session_maker.return_value = MagicMock()
+        current_game = Game(_id=31234, date='14/11/1990', word='Alura',
+                            find='Alu_a', tries=1, result='',
+                            time_sec=3213213213,
+                            )
+        mocking_session_maker()().query().filter().one.return_value = current_game
+        mocking_time.return_value = 3213213214
+
+        gess_word_return = gess_word(31234, 'R')
+
+        self.assertEqual(gess_word_return,
+                         {
+                             'game_id': 31234,
+                             'data': {
+                                 'find': 'Alura',
+                                 'tries': 1,
+                                 'result': 'win',
+                                 'time_sec': 3213213213,
+                                 'date': '14/11/1990',
+                                 'word': 'Alura'
+                             }
+                         }
+                         )
 
 if __name__ == "__main__":
     unittest.main()
 
-# class MockedObject(object):
-#
-#     def __setitem__(self, key, value):
-#         self.__dict__[key] = value
-#
-#     def __getitem__(self, key):
-#         return self.__dict__[key]
+
+class Game:
+    id: str
+    date: str
+    find: str
+    result: str
+    time_sec: float
+    tries: int
+    word: str
+
+    def __init__(self, _id, date, word, find, tries, result, time_sec):
+        self.id = _id
+        self.date = date
+        self.word = word
+        self.find = find
+        self.tries = tries
+        self.result = result
+        self.time_sec = time_sec
